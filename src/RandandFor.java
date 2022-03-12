f480bfe03f5d48fd065cfaaa2ece03836a8e87c1
KKK <ksniezek@protonmail.com>
2022-03-12 13:27:46 +0100

    Tablice

--- src/RandandFor.java
+++ src/RandandFor.java
@@ -2,7 +2,6 @@ import java.util.Random;
 
 public class RandandFor {
     public static void main(String[] args) {
-
     }
     /////TYPY LOSOWANIA LICZB/////
     //double random = Math.random() * 100;
@@ -32,6 +31,20 @@ public class RandandFor {
     //    a[0]+=2000;
     //    for(int i =0; i<a.length;i++){
     //        System.out.println(a[i]);
+    //    }
+    //}
+
+    //////Tworzenie tablicy od 1 do 10000
+    //int b[] = new int[10000];
+    //for(int i = 0;i<b.length;i++){
+    //    b[i]=i+1;
+    //}
+
+    ///////Dwuwymiarowe tablice
+    //int a[][]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    //    for(int i = 0; i<a.length;i++) {
+    //    for (int j = 0;j<a[i].length;j++){
+    //        System.out.println(a[i][j]);
     //    }
     //}
 }
