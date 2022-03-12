1c36540c828fb6bbbf31e9a3b7ecbbcc6cdbc81d
KKK <ksniezek@protonmail.com>
2022-03-12 11:57:22 +0100

    Kalkulator switch

---
+++ src/RandandFor.java
@@ -0,0 +1,12 @@
+public class RandandFor {
+    //double random = Math.random() * 99 + 1;
+    //int random = (int)(Math.random() * 100 + 1);
+
+    //Random rand = new Random();
+    //int n = rand.nextInt(50);
+    //n++;
+
+
+
+
+}
