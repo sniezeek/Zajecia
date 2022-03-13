fc80d62376a97a750ebcc079e1648434366a91c5
KKK <ksniezek@protonmail.com>
2022-03-13 10:49:26 +0100

    Rekurencja

--- src/zajecia2/KlasaPrywatnaProgram.java
+++ src/zajecia2/KlasaPrywatnaProgram.java
@@ -2,8 +2,17 @@ package zajecia2;
 
 public class KlasaPrywatnaProgram {
     public static void main(String[] args) {
-        KlasaPrywatna obj = new KlasaPrywatna();
-        obj.suma();
-        System.out.println(obj.rekurencja(800));
+        //KlasaPrywatna obj = new KlasaPrywatna();
+        //obj.suma();
+        //System.out.println(obj.rekurencja(800));
+        int a[] = {1, 2, 3};
+        try {
+            System.out.println(a[10]);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        System.out.println(a[0]);
+        System.out.println("Dalsza czesc programu");
+
     }
 }
