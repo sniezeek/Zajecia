eb440d491e785cd23927899bd7cb4594f1fe7d66
KKK <ksniezek@protonmail.com>
2022-03-13 10:44:06 +0100

    Rekurencja

--- src/zajecia2/KlasaPrywatna.java
+++ src/zajecia2/KlasaPrywatna.java
@@ -1,21 +1,37 @@
 package zajecia2;
 
+import java.util.Scanner;
+
 public class KlasaPrywatna {
     private int a;
+    private int b;
 
     KlasaPrywatna(){
-        this.a=100;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj liczbe a");
+        this.a=input.nextInt();
+        System.out.println("Podaj liczbe b");
+        this.b=input.nextInt();
     }
 
-    public void wypisanie_a(){
-        System.out.println(this.a);
-        powiekszanie_a();
-        System.out.println(this.a);
+    public void suma(){
+        System.out.println(this.a+this.b);
     }
-    public int get_a(){
-        return this.a;
+
+    //10! = 1*2*3*4....*10
+    //1+1+2+3+5+8
+    public int rekurencja(int n){
+        if(n>0){
+            return n+rekurencja(n-1);
+        }else{
+            return 0;
+        }
     }
-    private void powiekszanie_a(){
-        this.a+=10;
+    public int silnia(int n){
+        if(n>1){
+            return n*rekurencja(n-1);
+        }else{
+            return 1;
+        }
     }
 }
--- src/zajecia2/KlasaPrywatnaProgram.java
+++ src/zajecia2/KlasaPrywatnaProgram.java
@@ -3,7 +3,7 @@ package zajecia2;
 public class KlasaPrywatnaProgram {
     public static void main(String[] args) {
         KlasaPrywatna obj = new KlasaPrywatna();
-        obj.wypisanie_a();
-        //System.out.println(obj.get_a());
+        obj.suma();
+        System.out.println(obj.rekurencja(800));
     }
 }
